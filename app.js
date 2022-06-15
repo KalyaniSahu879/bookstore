2926a58aa5541f7a5d01d1f72d80c5029cba49b4
Kalyani Sahu <sahu.kalyani879@gmail.com>
2022-06-15 20:46:27 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+console.log("hello world");
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const bookRouter = require('./routes/book-routes')
+
+//middlewares
+
+app.use("/books" , bookRouter)
+
+mongoose.connect("mongodb+srv://nicky:nicky_123@cluster0.milck.mongodb.net/bookStore?retryWrites=true&w=majority")
+.then(()=>console.log("connected to the DB"))
+.then(()=>{
+    app.listen(5000)
+}).catch((e)=>console.log(e));
